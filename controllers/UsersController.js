c847ab067726a44c86aa0a8328819c889fa71959
Okinbird <okinyinka@yahoo.co.uk>
2023-06-26 12:01:12 -0700

    add new user to database

---
+++ controllers/UsersController.js
@@ -0,0 +1,24 @@
+import { createHash } from 'crypto';
+import dbClient from '../utils/db';
+
+class UsersController {
+  static async postNew(req, res) {
+    const { email, password } = req.body;
+    const users = await dbClient.database.collection('users');
+
+    if (!email) return res.status(400).send({ error: 'Missing email' });
+    if (!password) return res.status(400).send({ error: 'Missing password' });
+
+    const existingUser = await users.findOne({ email });
+    if (existingUser) return res.status(400).send({ error: 'Already exist' });
+
+    const hashedPwd = createHash('sha1').update(password).digest('hex');
+    const newUserData = { email, password: hashedPwd };
+    const result = await users.insertOne(newUserData);
+
+    return res.status(201).send({ id: result.insertedId, email });
+  }
+
+}
+
+export default UsersController;
