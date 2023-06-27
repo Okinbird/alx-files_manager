98d1240ccf27794830164fc15846687aa1a9767f
Okinbird <okinyinka@yahoo.co.uk>
2023-06-26 18:34:40 -0700

    update server file

--- server.js
+++ server.js
@@ -9,5 +9,5 @@ app.use(express.json());
 app.use('/', router);
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`Server running on port ${port}`);
 });
